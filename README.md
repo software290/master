d286c0a4682076e1b25751a384a848384a981843
software290 <mttopflag29@gmail.com.>
2024-03-23 11:46:59 +0900

    third update

---
+++ README.md
@@ -0,0 +1,13 @@
+# Admin template for React with material-ui
+
+[PREVIEW](http://manogel.github.io/template-material-ui) - Type any account to log in
+
+If you have any ideas for the page or want to improve our repository, leave your pull request and come with us. You will be contributing strongly to our react ❤️
+
+### This project aims to be simple and dynamic.
+
+- AJAX component: **[axios](https://github.com/axios/axios)**
+- UI component: **[material-ui](https://material-ui.com/)**
+- REACT stack: react, react-hooks, react-dom, react-router-dom@v6, **[unform](https://unform.dev/)**
+
+**📸 waiting screenshots 🙈**
---
+++ config-overrides.js
@@ -0,0 +1,51 @@
+const { override, addWebpackPlugin, addBabelPlugin } = require('customize-cra');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = override(
+  // isDevelopment && addWebpackPlugin(new ReactRefreshPlugin()),
+  addBabelPlugin([
+    'babel-plugin-root-import',
+    {
+      paths: [
+        {
+          rootPathSuffix: './src/layouts',
+          rootPathPrefix: '@layouts/',
+        },
+        {
+          rootPathSuffix: './src/pages',
+          rootPathPrefix: '@pages/',
+        },
+        {
+          rootPathSuffix: './src/contexts',
+          rootPathPrefix: '@contexts/',
+        },
+        {
+          rootPathSuffix: './src/styles',
+          rootPathPrefix: '@styles/',
+        },
+        {
+          rootPathSuffix: './src/assets',
+          rootPathPrefix: '@assets/',
+        },
+        {
+          rootPathSuffix: './src/components',
+          rootPathPrefix: '@components/',
+        },
+        {
+          rootPathSuffix: './src/validations',
+          rootPathPrefix: '@validations/',
+        },
+        {
+          rootPathSuffix: './src/utils',
+          rootPathPrefix: '@utils/',
+        },
+        {
+          rootPathSuffix: './src/services',
+          rootPathPrefix: '@services/',
+        },
+      ],
+    },
+  ]),
+);
